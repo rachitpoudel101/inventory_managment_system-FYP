1a7ec0c2c1c47913b92eaec8b86768e1394ec1c1
rachitpoudel101 <rachit300poudel@gmail.com>
2025-01-04 17:08:23 +0545

    i have made a new django project adn the different app like user dashboard and store.

--- Backend/inventory_man/inventory_man/asgi.py
+++ Backend/inventory_man/inventory_man/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for backend project.
+ASGI config for inventory_man project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory_man.settings')
 
 application = get_asgi_application()
